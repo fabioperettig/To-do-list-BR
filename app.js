f76925cb4fda654a538e8d4469c4939bf194e5f9
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2020-09-22 19:51:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 
 //Functions
-function addTodo(event) => {
+function addTodo(event){
     event.preventDefault(); //Impede reinicialização automática da página
-    console.log("Tarefa registrada!")
+    console.log("Tarefa registrada!");
 
     //Create Todo DIV
     const todoDiv = document.createElement("div");
